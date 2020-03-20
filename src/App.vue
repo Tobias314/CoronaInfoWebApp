fad479c3bee19097c4f0a7a5fdd1d967dc062d39
Finn Klein <finn.kl@icloud.com>
2020-03-20 22:18:12 +0100

    basic vue hello world

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <body>
+     <li v-for="item in themes" :key="item.id">
+    {{ item.name }}
+    </li>
+  </body>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: function() {
+    return {
+      themes: [{"name": "help me vllt habe ich corona", "id": 1}, {"name": "lol ich bin broke", "id": 2}, {"name": "wo bekomme ich gute wissenschaftliche infos?", "id": 3}],
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
