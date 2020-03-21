1c5ceb14705c4906cacb64d98c338f73d5633420
Finn Klein <finn.kl@icloud.com>
2020-03-21 11:53:36 +0100

    vue material weil es einfach schöner aussieht

--- frontend/src/components/list.vue
+++ frontend/src/components/list.vue
@@ -1,10 +1,23 @@
 <template>
   <body>
-    <li v-for="item in themes" :key="item.id">
-      <button @click="gotoinfo(item.link)"> 
-        {{ item.name }}
-      </button>
-    </li>
+    <div class="full-control">
+    <div class="list">
+
+      <md-list v-for="item in themes" :key="item.id">
+        <md-list-item md-expand :md-expanded.sync="item.expand">
+          <span class="md-list-item-text">{{item.name}}</span>
+
+          <md-list slot="md-expand">
+            <md-list-item class="md-inset" v-for="subtopic in item.subtopics" :key="subtopic.id" @click=gotoinfo(subtopic.link)>
+              {{ subtopic.name }}
+            </md-list-item>
+          </md-list>
+        </md-list-item>
+
+      </md-list>
+    </div>
+  </div>
+
   </body>
 </template>
 
@@ -16,9 +29,17 @@ export default {
   data: function() {
     return {
       themes: [
-        {"name": "Persönliche Gesundheit", "id": 1, "link": "/medical"}, 
-        {"name": "Rechtliche Informationen", "id": 2, "link": "/legal"},
-        {"name": "Informationen zum Virus", "id": 3, "link": "/virus"},
+        {name: "Persönliche Gesundheit", id: 1, expand: false, subtopics: [
+                                                                {name: "Habe ich Corona?", id: 11, link: "/medical"}, 
+                                                                {name: "Ich habe Corona, was sollte ich tun?", id: 12, link: "/medical"}, 
+                                                                {name: "Wo bekomme ich Hilfe bei psychischen Problemen?", id: 13, link: "/medical"}]}, 
+        {name: "Rechtliche Informationen", id: 2, expand: false, subtopics: [
+                                                                {name: "Darf ich noch raus?", id: 21, link: "/legal"}, 
+                                                                {name: "Woher bekomme ich finanzielle Unterstützung?", id: 22, link: "/legal"}]}, 
+        {name: "Informationen zum Virus", id: 3, expand: false, subtopics: [
+                                                                {name: "Was ist das für ein Virus?", id: 31, link: "/virus"}, 
+                                                                {name: "Was wird aktuell geforscht?", id: 32, link: "/virus"}, 
+                                                                {name: "Wie ist die Ausbreitung?", id: 33, link: "/virus"}]}, 
         ],
     }
   },
@@ -33,6 +54,39 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+  $list-width: 100%;
 
+  .full-control {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap-reverse;
+  }
+
+  .list {
+    width: $list-width;
+  }
+
+  .md-list {
+    width: 100%;
+    max-width: 100%;
+    display: inline-block;
+  }
+
+  .full-control > .md-list {
+    width: $list-width;
+    max-width: 100%;
+    height: 400px;
+    display: inline-block;
+    overflow: auto;
+    border: 1px solid rgba(#000, .12);
+    vertical-align: top;
+  }
+
+  .control {
+    min-width: 250px;
+    display: flex;
+    flex-direction: column;
+    padding: 16px;
+  }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-
 import router from "./routes.js";
+
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+
+Vue.use(VueMaterial)
 
 Vue.config.productionTip = false;
 
