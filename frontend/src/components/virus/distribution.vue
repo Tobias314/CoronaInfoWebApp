aed594cd80c447fe5f4dd1b5725d3b6e35c2a686
Finn Klein <finn.kl@icloud.com>
2020-03-21 21:39:12 +0100

    table view

--- frontend/src/components/virus/distribution.vue
+++ frontend/src/components/virus/distribution.vue
@@ -1,8 +1,23 @@
 <template>
   <body>
-     Exponentiell. Ja, Oma, das ist sehr schnell.
-     {{ this.$parent.location }}
-     {{casecount}}
+    <h3>
+      Wie ist die Ausbreitung?
+    </h3>
+    <div>
+    <md-table>
+      <md-table-row>
+        <md-table-head>Anzahl Fälle in {{casecount.type}} {{ this.$parent.location }}</md-table-head>
+        <md-table-head>Änderungsrate</md-table-head>
+        <md-table-head>Zeit</md-table-head>
+      </md-table-row>
+
+      <md-table-row>
+        <md-table-cell>{{casecount.number_cases}}</md-table-cell>
+        <md-table-cell>{{casecount.change_rate}}</md-table-cell>
+        <md-table-cell>{{casecount.time}}</md-table-cell>
+      </md-table-row>
+    </md-table>
+    </div>
   </body>
 </template>
 
