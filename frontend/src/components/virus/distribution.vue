d0079e0132f55f2f89aa0990009d7e1a886afc66
Finn Klein <finn.kl@icloud.com>
2020-03-21 19:15:47 +0100

    display local distribution information

--- frontend/src/components/virus/distribution.vue
+++ frontend/src/components/virus/distribution.vue
@@ -1,13 +1,31 @@
 <template>
   <body>
      Exponentiell. Ja, Oma, das ist sehr schnell.
+     {{ this.$parent.location }}
+     {{casecount}}
   </body>
 </template>
 
 <script>
+import Api from "../api.js";
 
 export default {
   name: 'general_info',
+
+  data:function(){
+    return{
+      casecount: "",
+    }
+  },
+
+  mounted(){
+    Api.get(`/district_case_number/${this.$parent.location}`)
+    .then(response => {
+      this.casecount = response.data;
+    }, error => {
+      console.error(error);
+    });
+  }
 }
 </script>
 
