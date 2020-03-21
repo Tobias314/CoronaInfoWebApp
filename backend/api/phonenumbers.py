f11f6536e035ff5eb56e198d266e09a82e42001c
Finn Klein <finn.kl@icloud.com>
2020-03-21 13:38:33 +0100

    refactoring

--- backend/api/phonenumbers.py
+++ backend/api/phonenumbers.py
@@ -4,6 +4,14 @@ router = APIRouter()
 
 #DATA
 PHONE_NUMBERS_HEALTH_DEPARTMENT = {'Potsdam' : "0815"}
+PHONE_NUMBERS_MENTAL_HEALTH = [
+    {"name": "Telefonseelsorge", "number": "0800-1110111 / 116123"},
+    {"name": "Kinder- und Jugendtelefon", "number": "0800-116111"},
+    {"name": "Elterntelefon", "number": "0800-1110550"},
+    {"name": "Hilfetelefon Häusliche Gewalt", "number": "0800-0116016"},
+    {"name": "Hilfetelefon Sexueller Missbrauch", "number": "0800-2255530"},
+    {"name": "Beratungsstelle des Lesben- und Schwulenverbandes", "number": "0721-8317953"},
+    ]
 
 @router.get("/phone_number/health_department/{city_name}")
 def read_item(city_name: str):
@@ -11,11 +19,4 @@ def read_item(city_name: str):
 
 @router.get("/phone_number/mentalhealth")
 def get_numbers():
-    return {"phone_numbers": [
-                {"name": "Telefonseelsorge", "number": "0800-1110111 / 116123"},
-                {"name": "Kinder- und Jugendtelefon", "number": "0800-116111"},
-                {"name": "Elterntelefon", "number": "0800-1110550"},
-                {"name": "Hilfetelefon Häusliche Gewalt", "number": "0800-0116016"},
-                {"name": "Hilfetelefon Sexueller Missbrauch", "number": "0800-2255530"},
-                {"name": "Beratungsstelle des Lesben- und Schwulenverbandes", "number": "0721-8317953"},
-    ]}
+    return {"phone_numbers": PHONE_NUMBERS_MENTAL_HEALTH}
