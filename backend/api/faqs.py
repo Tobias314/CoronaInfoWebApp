0d05491d2ac32e24ca370950ec4084efa3ba49ba
Tobias314 <pito.mailing@gmail.com>
2020-03-22 17:39:36 +0100

    Make faqs searchable.

--- backend/api/faqs.py
+++ backend/api/faqs.py
@@ -11,11 +11,14 @@ from database.faqdata import FaqsData
 router = APIRouter()
 
 
-@router.get("/faqs/filtered_faqs/{search_phrase}")
+@router.get("/faqs/filter_faqs/")
 def get_filtered_faqs(search_phrase: str, state: str = 'ALL', district: str = 'ALL'):
     faqsData = FaqsData()
-    results = faqsData.full_text_search(search_phrase)
-    return {'faqs' : results}
+    if search_phrase:
+        results = faqsData.full_text_search(search_phrase)
+        return {'faqs' : results}
+    else:
+        return get_faqs(state, district)
 
 @router.get("/faqs/get_faqs/")
 def get_faqs(state: str = 'ALL', district: str = 'ALL'):
--- frontend/src/components/faqs/faqs.vue
+++ frontend/src/components/faqs/faqs.vue
@@ -1,5 +1,11 @@
 <template>
   <body>
+    <div class="search-wrapper">
+        <input type="text" placeholder="Search FAQ.." v-model="search_phrase"/>
+		<button @click="search">Search</button>
+    </div>
+
+
     <div >
       <div>
 
@@ -25,12 +31,21 @@ export default {
   data: function() {
     return {
       faqs: [],
+      search_phrase: '',
     }
   },
 
   methods: {
     gotoinfo: function(link){
       this.$router.push(link); 
+    },
+    search: function(){
+        Api.get(`/faqs/filter_faqs/`, {params:{search_phrase: this.search_phrase}})
+        .then(response => {
+        this.faqs = response.data.faqs;
+        }, error => {
+        console.error(error);
+        });
     }
   },
 
@@ -97,5 +112,38 @@ export default {
   white-space: normal;
   background: lightgray;
 }
+
+.search-wrapper {
+    position: relative;
+    label {
+      position: absolute;
+      font-size: 12px;
+      color: rgba(0,0,0,.50);
+      top: 8px;
+      left: 12px;
+      z-index: -1;
+      transition: .15s all ease-in-out;
+    }
+    input {
+      padding: 4px 12px;
+      color: rgba(0,0,0,.70);
+      border: 1px solid rgba(0,0,0,.12);
+      transition: .15s all ease-in-out;
+      background: white;
+      &:focus {
+        outline: none;
+        transform: scale(1.05);
+        & + label  {
+          font-size: 10px;
+          transform: translateY(-24px) translateX(-12px);
+        }
+      }
+      &::-webkit-input-placeholder {
+          font-size: 12px;
+          color: rgba(0,0,0,.50);
+          font-weight: 100;
+      }
+    }
+  }
 
 </style>
